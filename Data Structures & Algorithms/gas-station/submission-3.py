c4cd790af9cd680c8654f7c05e3c86566a270919
cherry51015 <charani51015@gmail.com>
2026-08-19 15:52:05 +0530

    Add: gas-station - submission-3

---
+++ Data Structures & Algorithms/gas-station/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total=0
+        diff=0
+        fuel=0
+        start=0
+        for i in range(len(gas)):
+            diff=gas[i]-cost[i]
+            total+=diff
+            fuel+=diff
+            if fuel<0:
+                fuel=0
+                start=i+1
+        if total<0:
+            return -1
+        return start
+        
